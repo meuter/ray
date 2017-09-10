a0d8f328952d1ee4a427296a78395ef911bb379e
Cédric Meuter <cedric.meuter@gmail.com>
2017-09-10 08:57:23 +0200

    Trying to simplify API a bit to make the rectangle example even more convenient to write

--- samples/rectangle.cpp
+++ samples/rectangle.cpp
@@ -7,75 +7,35 @@
 
 using namespace ray::platform;
 using namespace ray::gl;
-using namespace ray::math;
 
-/******************************************************************************/
+static constexpr auto VERTEX_SHADER = GLSL(330, 
+    in  vec2 vertPosition;
+    void main() { gl_Position = vec4(vertPosition,0,1); }
+);
 
-class Quad 
-{
-public:
-    Quad() 
-    {
-        mVertexBuffer.load({
-            -0.5f, -0.5f,
-            -0.5f,  0.5f,
-             0.5f, -0.5f,
-             0.5f,  0.5f,
-        });
-        mVertexArray.setAttribute(ATTRIBUTE_2F_POSITION, mVertexBuffer);
-    }
-
-    void draw() const
-    {
-        mVertexArray.bind();
-        glDrawArrays(GL_TRIANGLE_STRIP, 0, mVertexBuffer.count());
-    }
-
-private:
-    VertexArray mVertexArray;
-    VertexBuffer<2, f32> mVertexBuffer;
-};
-
-/******************************************************************************/
-
-class QuadRenderer
-{    
-public:
-    QuadRenderer()
-    {
-        mShader.attach(FragmentShader(FRAGMENT_SHADER));
-        mShader.attach(VertexShader(VERTEX_SHADER));
-        mShader.bind(ATTRIBUTE_2F_POSITION, "vertPosition");
-        mShader.link();       
-    }
-
-    void render(const Quad &quad)
-    {
-        mShader.use();
-        quad.draw();
-    }
-private:
-    ShaderProgram mShader;
-
-    static constexpr auto VERTEX_SHADER = GLSL(330, 
-        in  vec2 vertPosition;
-        void main() { gl_Position = vec4(vertPosition,0,1); }
-    );
-    
-    static constexpr auto FRAGMENT_SHADER = GLSL(330,
-        in  vec3 fragColor;
-        out vec4 color;
-        void main() { color = vec4(1,1,1,1); }
-    );
-};
-
-/******************************************************************************/
+static constexpr auto FRAGMENT_SHADER = GLSL(330,
+    in  vec3 fragColor;
+    out vec4 color;
+    void main() { color = vec4(1,1,1,1); }
+);
 
 int main()
 {
     auto window   = Window(1920, 1080, "Rectangle Sample"); 
-    auto quad     = Quad();
-    auto renderer = QuadRenderer();
+    auto quadBuf  = VertexBuffer<2, float>{
+        -0.5f, -0.5f,
+        -0.5f,  0.5f,
+         0.5f, -0.5f,
+         0.5f,  0.5f
+    };
+    auto quad     = VertexArray();
+    quad.setAttribute(ATTRIBUTE_2F_POSITION, quadBuf);
+
+    auto shader = ShaderProgram();
+    shader.attach(VertexShader(VERTEX_SHADER));
+    shader.attach(FragmentShader(FRAGMENT_SHADER));
+    shader.bind(ATTRIBUTE_2F_POSITION, "vertPosition");
+    shader.link();       
 
     glClearColor(0.0f, 0.2f, 0.2f, 0.0f);    
 
@@ -83,7 +43,9 @@ int main()
     {
         glClear(GL_COLOR_BUFFER_BIT);
 
-        renderer.render(quad);
+        shader.use();
+        quad.bind();
+        glDrawArrays(GL_TRIANGLE_STRIP, 0, 4);
 
         window.pollEvents();
         window.swapBuffers();
