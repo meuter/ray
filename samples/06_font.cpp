90b0473c0a389827f064e6c11059dcf7d3831e10
Cédric Meuter <cedric.meuter@gmail.com>
2017-09-14 16:05:28 +0200

    Basic pasring of Roboto and rasterizing the glyoh for A

--- samples/06_font.cpp
+++ samples/06_font.cpp
@@ -1,6 +1,17 @@
 #include <ray/assets/Font.hpp>
+#include <cstdlib>
+
+using namespace ray::assets;
 
 int main()
 {
+    auto roboto = Font("res/fonts/Roboto-Regular.ttf", 50);
 
+    auto index   = roboto.getGlyphIndex('A');
+    auto metrics = roboto.getGlyphMetrics(index);
+    auto glyph   = roboto.rasterizeGlyph(index);
+
+    glyph.toPNG("A.png");
+
+    return EXIT_SUCCESS;
 }
