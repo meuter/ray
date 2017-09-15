b7232349314435a800afec68e333ddd099081f8e
Cédric Meuter <cedric.meuter@gmail.com>
2017-09-15 15:45:44 +0200

    The text sample now batches the drawcall once per renderText

--- samples/08_text.cpp
+++ samples/08_text.cpp
@@ -44,22 +44,37 @@ class TextRenderer
     struct Vertex { vec2 xy, uv; };
 
 public:
+    static constexpr auto MAX_LETTERS = 1024;
+    static constexpr auto N_VERTEX_PER_LETTERS = 4;
+    static constexpr auto N_FLOATS_PER_VERTEX  = sizeof(Vertex)/sizeof(float);
+    static constexpr auto N_FLOATS_PER_LETTER  = N_VERTEX_PER_LETTERS*N_FLOATS_PER_VERTEX;
+    static constexpr auto N_INDICES_PER_LETTER = 6;
+
     TextRenderer() : mShader(VERTEX_SHADER, FRAGMENT_SHADER) 
     {        
-        mVertexBuffer.load({
-            // position     tex coord
-            0.0f, 0.0f,   0.0f, 1.0f, // bottom left
-            0.0f, 0.0f,   0.0f, 0.0f, // top left 
-            0.0f, 0.0f,   1.0f, 1.0f, // bottom right
-            0.0f, 0.0f,   1.0f, 0.0f, // top right
-        });
+        mVertexBuffer.reserve(MAX_LETTERS * N_FLOATS_PER_LETTER, GL_STREAM_DRAW);
+        mIndexBuffer.reserve(MAX_LETTERS * N_INDICES_PER_LETTER);        
         mQuad.bindAttributeAtOffset(0, mShader.getAttribute<vec2>("vertPosition"), mVertexBuffer);
         mQuad.bindAttributeAtOffset(2, mShader.getAttribute<vec2>("vertTexCoord"), mVertexBuffer);            
-        mAllWhite.load(WHITE);
-
+        mQuad.bindIndices(mIndexBuffer);
         mTextColor = mShader.getUniform<vec4>("textColor");
         mQuadTexture = mShader.getUniform<sampler2D>("quadTexture");
         mTransform = mShader.getUniform<mat4>("transform");
+
+        auto mappedIndices = mIndexBuffer.map(GL_WRITE_ONLY);
+        auto index = 0;
+        for (auto l = 0; l < MAX_LETTERS; l += N_INDICES_PER_LETTER)        
+        {
+            (*mappedIndices++) = index+0; 
+            (*mappedIndices++) = index+1;
+            (*mappedIndices++) = index+2; 
+            (*mappedIndices++) = index+1; 
+            (*mappedIndices++) = index+2;
+            (*mappedIndices++) = index+3; 
+            index += 4;
+        }
+        mIndexBuffer.unmap();
+
     }
 
     static ivec2 getViewport()
@@ -69,32 +84,27 @@ public:
         return { viewPort[2], viewPort[3] };
     }
 
-    static std::u16string toUTF16(const std::string &u8Text)
-    {
-        return std::wstring_convert<std::codecvt_utf8_utf16<char16_t>, char16_t>{}.from_bytes(&u8Text[0]);    
-    }
-
-    vec2 renderText(const vec2 &pos, const BakedFont &font, const Color &color, const std::u16string &u16Text)
+    vec2 renderText(const vec2 &pos, const BakedFont &font, const Color &color, const std::string &u8Text)
     {
         auto cursor = pos;
-        for (auto codepoint: u16Text)
-            cursor = renderCodepoint(cursor, font, color, codepoint);
-        return cursor;
-    }
 
-    vec2 renderCodepoint(const vec2 &pos, const BakedFont &font, const Color &color, int codepoint)
-    {
-        auto glyph = font.getGlyph(codepoint);
+        auto quadVertices = reinterpret_cast<Vertex*>(mVertexBuffer.map(GL_WRITE_ONLY));
+        auto nLetters = 0;
+        for (auto codepoint: u8Text)
+        {
+            auto glyph = font.getGlyph(codepoint);
+            auto topLeft     = cursor + glyph.bb.min;
+            auto bottomRight = cursor + glyph.bb.max;
 
-        Vertex *quadVertices = (Vertex *)mVertexBuffer.map(GL_WRITE_ONLY);
+            (*quadVertices++) = { {bottomRight.x, topLeft.y},     {glyph.uv.max.x, glyph.uv.min.y} };
+            (*quadVertices++) = { {topLeft.x,     topLeft.y},     {glyph.uv.min.x, glyph.uv.min.y} };
+            (*quadVertices++) = { {bottomRight.x, bottomRight.y}, {glyph.uv.max.x, glyph.uv.max.y} };
+            (*quadVertices++) = { {topLeft.x,     bottomRight.y}, {glyph.uv.min.x, glyph.uv.max.y} };
 
-        auto topLeft     = pos + glyph.bb.min;
-        auto bottomRight = pos + glyph.bb.max;
-
-        quadVertices[0] = { {bottomRight.x, topLeft.y},     {glyph.uv.max.x, glyph.uv.min.y} };
-        quadVertices[1] = { {topLeft.x,     topLeft.y},     {glyph.uv.min.x, glyph.uv.min.y} };
-        quadVertices[2] = { {bottomRight.x, bottomRight.y}, {glyph.uv.max.x, glyph.uv.max.y} };
-        quadVertices[3] = { {topLeft.x,     bottomRight.y}, {glyph.uv.min.x, glyph.uv.max.y} };
+            cursor.x += glyph.advance;
+            nLetters += 1;
+        }            
+        mVertexBuffer.unmap();
 
         auto viewport = getViewport();
         mTransform.set({
@@ -104,32 +114,31 @@ public:
             0.0f, 0.0f, 0.0f, 1.0f
         });
 
-        mVertexBuffer.unmap();
-
         mQuadTexture.set(font.texture().bind(GL_TEXTURE0));
         mTextColor.set(color);
         mQuad.bind();
-        glDrawArrays(GL_TRIANGLE_STRIP, 0, 4);
+        glDrawElements(GL_TRIANGLES, N_INDICES_PER_LETTER * nLetters, GL_UNSIGNED_INT, 0);
 
-        return { pos.x + glyph.advance, pos.y };
+        return cursor;
     }
+
 
 private:
     ShaderProgram mShader;
     VertexArray mQuad;
     VertexBuffer<f32,4> mVertexBuffer;
-    Texture mAllWhite;
     Uniform<vec4> mTextColor;
     Uniform<sampler2D> mQuadTexture;
     Uniform<mat4> mTransform;
+    ElementBuffer mIndexBuffer;
 };
 
 
 int main()
 {
-    auto window   = Window(1920, 1080, "Font Sample");
+    auto window   = Window(1920, 1080, "Text Sample");
     auto loop     = GameLoop(window, 60);    
-    auto melso    = BakedFont("res/fonts/Roboto-Regular.ttf", 20);
+    auto melso    = BakedFont("res/fonts/Roboto-Regular.ttf", 30);
     auto renderer = TextRenderer();
     
     glClearColor(0.0f, 0.2f, 0.2f, 0.0f);    
@@ -140,9 +149,9 @@ int main()
     {   
         glClear(GL_COLOR_BUFFER_BIT);
 
-        auto text = fmt("average frame time = %1%msec", 1000*loop.averageFrameTime().count());
+        auto text = fmt("average frame time = %6.3fmsec", 1000*loop.averageFrameTime().count());
 
-        renderer.renderText(vec2(0,0), melso, YELLOW, renderer.toUTF16(text));
+        renderer.renderText(vec2(0,0), melso, YELLOW, text);
     });
 
     return EXIT_SUCCESS;
