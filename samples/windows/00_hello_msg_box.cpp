98969614dacb93ccf5b090acf4fafec04b6c796a
Cédric Meuter <cedric.meuter@gmail.com>
2018-10-20 09:23:15 +0200

    Started adding hello world for windows

---
+++ samples/windows/00_hello_msg_box.cpp
@@ -0,0 +1,7 @@
+#include <windows.h>
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nShowCmd)
+{
+    MessageBox(NULL, "Hello World!", "Just another Hello World program!",MB_ICONEXCLAMATION | MB_OK);
+    return 0;
+}
